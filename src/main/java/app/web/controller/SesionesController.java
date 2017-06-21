0663f49ff4379ac3add8e37fd626f8164b1e65ce
Rodrigo Ballestero <ballesterorj@gmail.com>
2017-06-21 00:19:12 -0300

    Modificando el annotation del controlador de sesiones.

--- src/main/java/app/web/controller/SesionesController.java
+++ src/main/java/app/web/controller/SesionesController.java
@@ -5,12 +5,11 @@ import app.model.odb.Sesion;
 import app.service.SesionesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/api/authentication")
 public class SesionesController {
