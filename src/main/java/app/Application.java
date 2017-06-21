1cd619ae0eec2a52c3fcb1e2f134179213e348c5
Rodrigo Ballestero <ballesterorj@gmail.com>
2017-06-20 22:09:24 -0300

    Agregando Mongo.

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-//import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 
 @SpringBootApplication
@@ -16,7 +17,7 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 //        mostrarAsciiArtInicial();
-//        levantarMongo();
+        levantarMongo();
     }
 
     @Override
@@ -27,10 +28,10 @@ public class Application extends SpringBootServletInitializer {
     private static Class<Application> applicationClass = Application.class;
 
 
-//    private static void levantarMongo() {
-//        ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
-//        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
-//    }
+    private static void levantarMongo() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
+        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+    }
 
 //    private static void mostrarAsciiArtInicial() {
 //        final File workspacePath = getWorkspacePath();
@@ -43,7 +44,7 @@ public class Application extends SpringBootServletInitializer {
 //        }
 //
 //    }
-
+//
 //    private static File getWorkspacePath() {
 //        return new File(".").getAbsoluteFile();
 //    }
