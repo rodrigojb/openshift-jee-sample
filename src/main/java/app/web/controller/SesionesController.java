3955255b53191ff2bffff58b3144d84fec48e970
Rodrigo Ballestero <ballesterorj@gmail.com>
2017-06-23 21:48:29 -0300

    Probando CORS.

--- src/main/java/app/web/controller/SesionesController.java
+++ src/main/java/app/web/controller/SesionesController.java
@@ -4,7 +4,6 @@ import app.model.odb.Credencial;
 import app.model.odb.Sesion;
 import app.service.SesionesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -17,7 +16,7 @@ public class SesionesController {
     @Autowired
     SesionesService servicioDeSesiones;
 
-    @RequestMapping(value="/login", method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value="/login", method= RequestMethod.POST)
     @ResponseBody
     public Sesion loguearUsuario(@RequestBody Credencial userAndPassword) throws IOException{
         return servicioDeSesiones.loguearUsuario(userAndPassword);
