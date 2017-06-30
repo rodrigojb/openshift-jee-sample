37681b7103e522237d015563513857c9c630f6fb
Rodrigo Ballestero <ballesterorj@gmail.com>
2017-06-29 21:59:05 -0300

    Add webConfig.

--- src/main/java/app/WebConfig.java
+++ src/main/java/app/WebConfig.java
@@ -1,16 +1,26 @@
-//package app;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig extends WebMvcConfigurerAdapter {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**");
-//    }
-//}
+package app;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+@PreMatching
+public class WebConfig implements ContainerResponseFilter {
+
+    @Override
+    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
+
+        if ( requestCtx.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            requestCtx.abortWith( Response.status( Response.Status.OK ).build() );
+        }
+
+        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS" );
+    }
+}
