5a8b6eb54ef188abaed4e64b35b6a36ecbcc58b3
Rodrigo Ballestero <ballesterorj@gmail.com>
2017-06-30 17:41:37 -0300

    Probando configuracion.

--- src/main/java/app/WebConfig.java
+++ src/main/java/app/WebConfig.java
@@ -1,26 +1,29 @@
 package app;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.container.PreMatching;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.Provider;
-import java.io.IOException;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@Provider
-@PreMatching
-public class WebConfig implements ContainerResponseFilter {
+/**
+ * Created by aye on 30/06/17.
+ */
+@Configuration
+public class WebConfig {
 
-    @Override
-    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
-
-        if ( requestCtx.getRequest().getMethod().equals( "OPTIONS" ) ) {
-            requestCtx.abortWith( Response.status( Response.Status.OK ).build() );
-        }
-
-        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
-        responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
-        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS" );
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("*", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
