3dff2f65867475d3136499ade9db0882f2a0b193
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2017-07-04 17:52:46 -0300

    me quiero matar, estaba harckodeada la otra URL

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -1,4 +1,4 @@
 var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap'])
 
-.constant("BASE", "http://tacs-tmdb-tmdb-grupo1.1d35.starter-us-east-1.openshiftapps.com/api/");
+.constant("BASE", "http://tacs-tmdb-tmdb-grupo1.7e14.starter-us-west-2.openshiftapps.com/");
 //.constant("BASE", "http://localhost:8080/api/");
