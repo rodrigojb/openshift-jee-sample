865b3f747f2fb00304c8a161fd96ba089353188a
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2017-07-04 18:08:26 -0300

    api que falto

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -1,4 +1,4 @@
 var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap'])
 
-.constant("BASE", "http://tacs-tmdb-tmdb-grupo1.7e14.starter-us-west-2.openshiftapps.com/");
+.constant("BASE", "http://tacs-tmdb-tmdb-grupo1.7e14.starter-us-west-2.openshiftapps.com/api/");
 //.constant("BASE", "http://localhost:8080/api/");
